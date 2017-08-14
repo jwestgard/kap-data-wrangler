0fd83184fb8a0b5c7000362bd778b7b4826478ee
jwestgard <jwestgard@mac.com>
2017-08-14 13:13:49 -0400

    handle directory traversal and filename parsing

--- kdw.py
+++ kdw.py
@@ -22,22 +22,48 @@ def all_files(start_dir):
 class asset:
     '''Class representing an digital asset'''
     def __init__(self, input):
-        self.path = os.path.abspath(input)
+        self.path      = os.path.abspath(input)
         self.extension = os.path.splitext(input)[1]
-        self.basename = os.path.basename(input)
-        self.filename = self.basename.rsplit('.')[0]
-        (self.project, self.filnum, self.component) = self.filename.split('-')
+        self.basename  = os.path.basename(input)
+        self.filename  = self.basename.rsplit('.')[0]
         
+        filename_components = self.filename.split('-')
+        
+        if len(filename_components) == 3:
+            self.project   = filename_components[0]
+            self.serial_no = filename_components[1]
+            self.part_no   = filename_components[2]
+            self.itemname  = "{0}-{1}".format(self.project, self.serial_no)
+        else:
+            self.project   = None
+            self.serial_no = None
+            self.part_no   = None
+            self.itemname  = None
 
 
 def main():
     print_banner()
     path_to_check = os.path.abspath(sys.argv[1])
     print('Checking "{0}"'.format(path_to_check))
+    results = {'extra': []}
+
     for f in all_files(path_to_check):
-        print(f)
         a = asset(f)
-        print(a.project, a.filnum, a.component, a.extension)
+        if a.itemname is None:
+            results['extra'].append(a)
+        else:
+            if a.itemname in results:
+                results[a.itemname].append(a)
+            else:
+                results[a.itemname] = [a]
+
+    for item, assets in [i for i in results.items() if i != 'extra']:
+        components = set([a.part_no for a in assets])
+        print("{0} -- {1} pages / {2} files".format(
+            item, len(components), len(assets)
+            ))
+        for n in assets:
+            print("  -> {0}".format(n.basename))
 
 if __name__ == "__main__":
     main()
